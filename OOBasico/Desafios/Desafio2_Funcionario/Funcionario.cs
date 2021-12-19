8715674129a2115655ed321f53337e1773ad4492
braziltaiany <braziltaiany@outlook.com>
2021-12-19 15:23:17 -0300

    Adicionando desafio2

---
+++ OOBasico/Desafios/Desafio2_Funcionario/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Desafio2_Funcionario
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto, Imposto;
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += SalarioBruto * (porcentagem / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return $"" +
+                $"{Nome}, R$" +
+                $"{SalarioLiquido().ToString("F2",CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
---
+++ OOBasico/Desafios/Desafio2_Funcionario/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Desafio2_Funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine());
+            funcionario.SalarioLiquido();
+           
+            Console.WriteLine("\n\nFuncionário:" + funcionario);
+            
+            Console.Write("\n\nDigite a porcentagem para aumentar o salário: ");            
+            double percentual = double.Parse(Console.ReadLine());
+            
+            funcionario.AumentarSalario(percentual);           
+            
+            Console.WriteLine("\nDados atualizados:" + funcionario);
+        }
+    }
+}
