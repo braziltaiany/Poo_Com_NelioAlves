f3ff42b9507bc3fe64612f143a8aea0b81df333d
braziltaiany <braziltaiany@outlook.com>
2021-12-19 13:25:36 -0300

    Adicionando Método - conhecendo override ToString

--- OOBasico/EstoqueDeProdutos/EstoqueDeProdutos/Produto.cs
+++ OOBasico/EstoqueDeProdutos/EstoqueDeProdutos/Produto.cs
@@ -6,5 +6,21 @@ namespace EstoqueDeProdutos
 {
     class Produto
     {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public override string ToString()
+        {
+            return
+                $"{Nome}, " +
+                $"R${Preco.ToString(("F2"), CultureInfo.InvariantCulture)}, " +
+                $"{Quantidade} unidades, Total: R$" +
+                $"{ValorTotalEmEstoque().ToString(("F2"), CultureInfo.InvariantCulture)}";
+        }
     }
 }
--- OOBasico/EstoqueDeProdutos/EstoqueDeProdutos/Program.cs
+++ OOBasico/EstoqueDeProdutos/EstoqueDeProdutos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace EstoqueDeProdutos
 {
@@ -6,7 +7,17 @@ namespace EstoqueDeProdutos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade do produto: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.Write("Dados do produto: " + p);//método override com ToString por isso não preciso chamar cada atributo
         }
     }
 }
