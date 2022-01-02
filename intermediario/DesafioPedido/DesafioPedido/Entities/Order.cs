bea5dbb4a8037a2e49d26c7380bc4aa2ed58f8db
Taiany Brazil <braziltaiany@outlook.com>
2022-01-02 18:53:02 -0300

    Adicionando classe principal do pedido

---
+++ intermediario/DesafioPedido/DesafioPedido/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DesafioPedido.Entities;
+
+namespace DesafioPedido.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        
+    }
+}
