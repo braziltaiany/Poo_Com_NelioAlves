605144d3ed35c849ee8bb10e9b80005f263c0a42
Taiany Brazil <braziltaiany@outlook.com>
2022-01-02 13:20:04 -0300

    Adicionando funcionalidade de somar valor total dos contratos por funcionario

--- intermediario/WorkerContracts/WorkerContracts/Entities/Worker.cs
+++ intermediario/WorkerContracts/WorkerContracts/Entities/Worker.cs
@@ -30,5 +30,17 @@ namespace WorkerContracts.Entities
             Contracts.Remove(contract);
         }
 
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach(HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
+            return sum;
+        }
     }
 }
