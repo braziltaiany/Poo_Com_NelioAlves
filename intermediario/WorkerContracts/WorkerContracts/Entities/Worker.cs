86cfd13dbe4ae91e181ed81227cf46136961e040
Taiany Brazil <braziltaiany@outlook.com>
2022-01-02 12:58:30 -0300

    Adicionando propriedades na classe Worker

--- intermediario/WorkerContracts/WorkerContracts/Entities/Worker.cs
+++ intermediario/WorkerContracts/WorkerContracts/Entities/Worker.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WorkerContracts.Entities.Enums;
 
 namespace WorkerContracts.Entities
 {
     class Worker
     {
         public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+
+
     }
 }
