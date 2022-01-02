757c2c1b5348f4c8ab4e14626be2196d99142caa
Taiany Brazil <braziltaiany@outlook.com>
2022-01-02 18:40:56 -0300

    Adicionando Classe cliente

---
+++ intermediario/DesafioPedido/DesafioPedido/Entities/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioPedido.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+    }
+}
