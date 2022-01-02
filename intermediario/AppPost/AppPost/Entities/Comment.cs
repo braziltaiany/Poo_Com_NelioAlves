58f512c08a4e48361b9ee73adf57d28102a589b9
Taiany Brazil <braziltaiany@outlook.com>
2022-01-02 18:10:08 -0300

    Adicionando AppPost

---
+++ intermediario/AppPost/AppPost/Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿namespace AppPost.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+
+}
---
+++ intermediario/AppPost/AppPost/Entities/Post.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppPost.Entities
+{
+    class Post
+    {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post(DateTime date, string title, string content, int likes)
+        {
+            Date = date;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+    }
+}
