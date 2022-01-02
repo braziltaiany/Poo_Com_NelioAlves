fedeb3abdb3f44b6c2c89d69ca0891bd3b67aa68
Taiany Brazil <braziltaiany@outlook.com>
2022-01-02 12:25:04 -0300

    Adicionando Classe HourContract e seus Construtores

---
+++ intermediario/WorkerContracts/WorkerContracts/Entities/HourContract.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkerContracts.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+    }
+}
