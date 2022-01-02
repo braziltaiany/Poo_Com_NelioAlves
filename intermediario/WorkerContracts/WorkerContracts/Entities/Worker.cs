5bbef4613ba6556077f6827a9b33274973a5409b
Taiany Brazil <braziltaiany@outlook.com>
2022-01-02 12:49:39 -0300

    Adicionando Classe Worker

---
+++ intermediario/WorkerContracts/WorkerContracts/Entities/Worker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkerContracts.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+    }
+}
