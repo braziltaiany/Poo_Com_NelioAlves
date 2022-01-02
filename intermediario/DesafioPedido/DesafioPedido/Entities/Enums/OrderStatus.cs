2164d36de623c7a426131b6f9a9204482810fc30
Taiany Brazil <braziltaiany@outlook.com>
2022-01-02 18:56:11 -0300

    Adicionando enum do status do pedido

---
+++ intermediario/DesafioPedido/DesafioPedido/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioPedido.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment,
+        Processing,
+        Shipped,
+        Delivered
+    }
+}
