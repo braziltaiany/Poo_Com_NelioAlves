bb625d73156f1aff2a82d420903c5dec9749b112
Taiany Brazil <braziltaiany@outlook.com>
2022-01-02 18:50:38 -0300

    Adicionando funcionalidade do subtotal

--- intermediario/DesafioPedido/DesafioPedido/Entities/OrderItem.cs
+++ intermediario/DesafioPedido/DesafioPedido/Entities/OrderItem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DesafioPedido.Entities;
 
 namespace DesafioPedido.Entities
 {
@@ -8,9 +9,21 @@ namespace DesafioPedido.Entities
     {
         public int Quantify { get; set; }
         public double Price { get; set; }
+        public List<Products> Product { get; set; } = new List<Products>();
 
         public OrderItem()
         {
+        }
+
+        public OrderItem(int quantify, double price)
+        {
+            Quantify = quantify;
+            Price = price;
+        }
+
+        public double subTotal()
+        {
+            return Quantify * Price;
         }
     }
 }
--- intermediario/DesafioPedido/DesafioPedido/Entities/Product.cs
+++ intermediario/DesafioPedido/DesafioPedido/Entities/Product.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace DesafioPedido.Entities
 {
-    class Product
+    class Products
     {
         public string Name { get; set; }
         public double Price { get; set; }
