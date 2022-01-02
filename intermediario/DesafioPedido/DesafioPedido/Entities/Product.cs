0e1221bd318aca2ff471a43f7593ec14c1a7fbc7
Taiany Brazil <braziltaiany@outlook.com>
2022-01-02 18:46:27 -0300

    Adicionando Classe produto

---
+++ intermediario/DesafioPedido/DesafioPedido/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioPedido.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
