c6e4c8fc7926a763940f088df77497e40f1f7e37
Taiany Brazil <braziltaiany@outlook.com>
2022-01-02 18:44:36 -0300

    Adicionando Classe item do pedido

---
+++ intermediario/DesafioPedido/DesafioPedido/Entities/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioPedido.Entities
+{
+    class OrderItem
+    {
+        public int Quantify { get; set; }
+        public double Price { get; set; }
+
+        public OrderItem()
+        {
+        }
+    }
+}
