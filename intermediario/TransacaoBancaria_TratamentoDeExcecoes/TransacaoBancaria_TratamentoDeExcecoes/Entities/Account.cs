40ed1fba2b0bea3e5b1c927b6ac7aecf95196b50
Taiany Brazil <braziltaiany@outlook.com>
2022-01-09 11:10:46 -0300

    Adicionando classe Account

---
+++ intermediario/TransacaoBancaria_TratamentoDeExcecoes/TransacaoBancaria_TratamentoDeExcecoes/Entities/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransacaoBancaria_TratamentoDeExcecoes.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance,double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
---
+++ intermediario/TransacaoBancaria_TratamentoDeExcecoes/TransacaoBancaria_TratamentoDeExcecoes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TransacaoBancaria_TratamentoDeExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
